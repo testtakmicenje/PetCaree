dd982cb3978282176fb2799b761c6997c6a19d5a
PC <dejanaskoric71@gmail.com>
2024-01-24 21:40:24 +0100

    new

--- app/src/main/java/com/example/petcare/settings/SettingsActivity.java
+++ app/src/main/java/com/example/petcare/settings/SettingsActivity.java
@@ -1,12 +1,11 @@
 package com.example.petcare.settings;
 
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.content.Intent;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,13 +15,9 @@ import com.example.petcare.appintro.AppIntro;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private static final String PREFS_NAME = "MyPrefsFile";
-    private static final String NOTIFICATIONS_ENABLED_KEY = "notifications_enabled";
-    private static final String TOAST_SHOWN_KEY = "toast_shown";
 
-    private Switch notificationsSwitch;
-    private boolean lastNotificationStatus;
-    private boolean toastShown;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +37,6 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
         TextView aboutAppTextView = findViewById(R.id.aboutAppTextView);
-
-
         aboutAppTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,52 +55,5 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-        notificationsSwitch = findViewById(R.id.notificationsSwitch);
-
-
-        notificationsSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked != lastNotificationStatus) {
-
-                if (!toastShown) {
-                    SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-                    SharedPreferences.Editor editor = settings.edit();
-                    editor.putBoolean(NOTIFICATIONS_ENABLED_KEY, isChecked);
-                    editor.apply();
-
-
-                    showToast(isChecked ? "Obavještenja su uključena." : "Obavještenja su isključena.");
-
-
-                    toastShown = true;
-                }
-
-              lastNotificationStatus = isChecked;
-            }
-        });
-
-
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        boolean notificationsEnabled = settings.getBoolean(NOTIFICATIONS_ENABLED_KEY, true);
-        notificationsSwitch.setChecked(notificationsEnabled);
-
-
-        toastShown = settings.getBoolean(TOAST_SHOWN_KEY, false);
-    }
-
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putBoolean(TOAST_SHOWN_KEY, toastShown);
-        editor.apply();
     }
 }
